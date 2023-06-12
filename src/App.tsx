cf1932761e02ed464cf56842434d1ad6dc151731
Luis Acosta <lacostat@cjf.gob.mx>
2023-06-11 19:04:37 -0600

    Se ajustan estilos

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,23 @@ import { useBattery } from './hooks/useBattery';
 
 function App() {
 
-  const { bettery, charging } = useBattery();
+  const { bettery, charging, sentMessage, permissionNotification } = useBattery();
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Battery Level: {bettery}%</h1>
+        <div className="contenido-title">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Battery Level: {bettery}%</h1>
+        </div>
         <h1>Charging state changed: {charging ? 'Charging' : 'Not charging'}</h1>
+        <h2>⚠️Make sure to enable browser permissions: background sync.⚠️</h2>
+        {
+          !permissionNotification && <h2>The permissions to notify are not enabled.</h2>
+        }
+        {/* <button onClick={()=> sentMessage('Prueba', '1218607412')}>Salida</button>
+        <button onClick={()=> sentMessage('Prueba', '6206194345')}>Casa</button> */}
+        <p>Con telegram</p>
       </header>
     </div>
   );
